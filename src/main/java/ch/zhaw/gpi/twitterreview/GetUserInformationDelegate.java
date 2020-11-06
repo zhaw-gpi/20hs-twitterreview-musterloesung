da09a1487eace25c1a06e7e70e56242e38b79434
Björn Scheppler <scep@zhaw.ch>
2020-11-06 17:56:52 +0100

    Bis und mit Kapitel 9

--- src/main/java/ch/zhaw/gpi/twitterreview/GetUserInformationDelegate.java
+++ src/main/java/ch/zhaw/gpi/twitterreview/GetUserInformationDelegate.java
@@ -4,6 +4,11 @@ import javax.inject.Named;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.json.JSONObject;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 
 
@@ -15,9 +20,20 @@ public class GetUserInformationDelegate implements JavaDelegate {
     public void execute(DelegateExecution execution) throws Exception {
         String userName = (String) execution.getVariable("anfrageStellenderBenutzer");
 
-        
+        RestTemplate restTemplate = new RestTemplate();
 
-        execution.setVariable("userFullName", "Mr. X");
+        ResponseEntity<String> response = restTemplate.exchange("http://localhost:8070/api/users/{userName}", HttpMethod.GET, null, String.class, userName);
+
+        String fullName;
+
+        if(response.getStatusCode().equals(HttpStatus.OK)){
+            JSONObject userAJsonObject = new JSONObject(response.getBody());
+            fullName = userAJsonObject.getString("firstName") + " " + userAJsonObject.getString("officialName");
+        } else {
+            fullName = "Mr. X";
+        }
+
+        execution.setVariable("userFullName", fullName);
 
     }
     
